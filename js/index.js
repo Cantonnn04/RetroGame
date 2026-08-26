eb48c98be2cd7ae2591bc604e140eeb7d2b4be7f
Cantonnn04 <antongrinev04@gmail.com>
2026-08-26 16:08:23 -0400

    Delete js directory

--- js/index.js
+++
@@ -1,5 +0,0 @@
-const games = ['snake.html', 'minesweeper.html', 'brickbreaker.html', 'platformer.html', 'pong.html'];
-function goRandom() {
-  const pick = games[Math.floor(Math.random() * games.length)];
-  window.location.href = pick;
-}
